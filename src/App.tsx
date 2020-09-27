0d2c9217f4199b41b2542194f693cfe2d163745e
Lucas Cavalcante <lcdaponte@gmail.com>
2020-09-27 09:39:58 -0300

    Added global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <h1>Teste</h1>;
-};
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>TESTE</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
